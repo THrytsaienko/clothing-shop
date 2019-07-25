34f87c335ace1cdfd70cd06e3a7c9335d9f980e4
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2019-07-25 15:02:24 +0300

    Implemented firebase utils, auth added to firebase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import { auth } from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 import Header from './components/header/header';
 import HomePage from './pages/homepage/homepage';
@@ -21,11 +21,21 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user });
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if(userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
 
-            console.log('user >>>', user);
-        })
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+                });
+            }
+            this.setState({ currentUser: userAuth });
+        });
     }
 
     componentWillUnmount() {
