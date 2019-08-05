57aab56df52532a7f9c0928c03f2a20959aa16df
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2019-08-05 15:04:54 +0300

    Empty cart message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
@@ -9,6 +11,7 @@ import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import Header from './components/header/header';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import CheckoutPage from './pages/checkout/checkout';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 class App extends React.Component {
@@ -42,6 +45,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
+                    <Route exact path='/checkout' component={CheckoutPage}/>
                     <Route exact path='/signin'
                            render={() =>
                                this.props.currentUser ?
@@ -54,8 +58,8 @@ class App extends React.Component {
     }
 }
 
-const mapStateToProps = ({ user }) => ({
-    currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -2,24 +2,30 @@ import React from 'react';
 import './cart-dropdown.scss';
 import CustomButton from '../custom-button/custom-button';
 import CartItem from '../cart-item/cart-item';
-import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import { connect } from 'react-redux';
 
 const CartDropdown = ({cartItems}) => (
-        <div className='cart-dropdown'>
-            <div className='cart-items'>
-                {
+    <div className='cart-dropdown'>
+        <div className='cart-items'>
+            {
+                cartItems.length ? (
                     cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
-                }
-            </div>
-            <CustomButton>GO TO CHECKOUT</CustomButton>
+                    ) : (
+                        <span className='empty-message'>Your cart is empty</span>
+                )
+
+
+
+            }
         </div>
-    );
+        <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+);
 
 
-const mapStateToProps = state => ({
-    cartItems: selectCartItemsCount(state)
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
 });
 
 export default connect(mapStateToProps)(CartDropdown);
